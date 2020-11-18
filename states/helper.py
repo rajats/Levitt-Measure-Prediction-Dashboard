cb3ec59f54b1af2b19b0a3799bfe74c8717282ac
rajats <rajat_kumar@outlook.com>
2020-11-18 21:10:51 +0530

    docstring for state helper functions, improved naming

--- states/helper.py
+++ states/helper.py
@@ -19,7 +19,7 @@ state_code_dict = {'Maharashtra': 'mh', 'Gujrat': 'gj', 'Goa': 'ga', 'Tamil Nadu
                    'Puducherry': 'py'}
 
 # dictionary to map state codes to state names
-reverse_state_code_dict = {v: k for k, v in state_code_dict.items()}
+reverse_state_code_dict = {state_code: state_name for state_name, state_code in state_code_dict.items()}
 
 # get data from api and store it in dataframe
 url = 'https://api.covid19india.org/states_daily.json'
@@ -29,24 +29,34 @@ df_all_states = pd.DataFrame(dict_req['states_daily'])
 df_all_states['date'] = pd.to_datetime(df_all_states['date'])
 
 
-# get daily confirmed, recovered and deceased cases for a state
-def get_confirmed_recovered_deceased(df, state):
-    con = df[df['status'] == 'Confirmed'][['date', state]]
+def get_confirmed_recovered_deceased(df, state_code):
+    """
+    get the daily confirmed, recovered and deceased cases for a state
+    :param df: unstructured dataframe of a state containing date and daily confirmed, recovered, deceased cases
+    in three rows
+    :param state_code: two letter code associated with each state
+    :return: three dataframes containing daily confirmed, recovered, deceased cases of a state
+    """
+    con = df[df['status'] == 'Confirmed'][['date', state_code]]
     con.columns = ['date', 'confirmed']
-    rec = df[df['status'] == 'Recovered'][['date', state]]
-    rec.columns = ['date1', 'recovered']
-    dec = df[df['status'] == 'Deceased'][['date', state]]
-    dec.columns = ['date1', 'deceased']
+    rec = df[df['status'] == 'Recovered'][[state_code]]
+    rec.columns = ['recovered']
+    dec = df[df['status'] == 'Deceased'][[state_code]]
+    dec.columns = ['deceased']
     return con, rec, dec
 
 
-# get dataframe for a state containing required parameters
 def get_state_df(state_code):
+    """
+    creates dataframe for a state containing required parameters
+    :param state_code: two letter code associated with each state
+    :return: structured dataframe for a state
+    """
     df_state = df_all_states[['date', state_code, 'status']].copy()
     con, rec, dec = get_confirmed_recovered_deceased(df_state, state_code)
     df_state_daily = pd.concat([con.reset_index(drop=True), rec.reset_index(drop=True), dec.reset_index(drop=True)],
                                axis=1)
-    df_state_daily = df_state_daily[['date', 'confirmed', 'recovered', 'deceased']].copy()
+    df_state_daily = df_state_daily.copy()
     df_state_daily['confirmed'] = pd.to_numeric(df_state_daily['confirmed'])
     df_state_daily['recovered'] = pd.to_numeric(df_state_daily['recovered'])
     df_state_daily['deceased'] = pd.to_numeric(df_state_daily['deceased'])
